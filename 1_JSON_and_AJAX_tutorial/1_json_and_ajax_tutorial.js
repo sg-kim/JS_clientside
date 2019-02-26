b18eb2ff65fe3646932436bb1252cc1087dbe7c5
Seunggu Kim <sgkim08@gmail.com>
2019-02-27 05:42:14 +0900

    port# changed: 3000 --> 3001

--- 1_JSON_and_AJAX_tutorial/1_json_and_ajax_tutorial.js
+++ 1_JSON_and_AJAX_tutorial/1_json_and_ajax_tutorial.js
@@ -1,8 +1,8 @@
 
 var app = require('./externals/express.js')();
 
-app.listen(3000, function(){
-	console.log('connected to 3000 port!');
+app.listen(3001, function(){
+	console.log('connected to 3001 port!');
 });
 
 var content = require('./routes/content')();
